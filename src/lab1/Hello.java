b7e1c5a225960ef3b78c84ee3d4724ed5868c3b3
momay <kotchamon.momay@gmail.com>
2019-12-08 11:31:08 +0700

    Add name

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -4,10 +4,11 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World,I a Kotchamon");
+        System.out.println("Hello World,I a Kotchamon Ketin");
+        System.out.println("361211760001");
         System.out.println("MIT");
         System.out.println("Faculty of Management Technology");
-        System.out.println("Saiyai");
+        System.out.println("Rajamangala University of Technology Saiyai");
 
     }//main
 
